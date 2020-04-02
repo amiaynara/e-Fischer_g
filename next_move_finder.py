bf359781ab17b7f4ecadb4d30d2214fb3c6171b3
amiay <amiayn@iitbhilai.ac.in>
2020-04-03 04:33:03 +0530

    first step: online move finder script

---
+++ next_move_finder.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+file_location='/home/hduser/test_chess'
+browser=webdriver.Chrome()
+browser.implicitly_wait(30)
+browser.get('https://nextchessmove.com')
+pgn_link=browser.find_element_by_link_text('PGN')
+pgn_link.click()
+data=browser.find_element_by_xpath('//input[@type="file"]').send_keys(file_location) # xpath and class find the same element
+confirm_upload=browser.find_element_by_link_text('Save To Board')
+# select the chess file for the current position
+confirm_upload.click()
+
+#board loaded 
+calculate_move=browser.find_element_by_id('calculate-button')
+calculate_move.click()
+check_element=browser.find_element_by_link_text('Stockfish 11')
+the_move_element=browser.find_elements_by_xpath('//a[@href="#"]')[7] # Don't forget to use 's'
+the_move_element.click()
+#print(the_move_element.get_attribute("innerHTML"))
+print(the_move_element.text)
+
