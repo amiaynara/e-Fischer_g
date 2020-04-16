5739ef9c7b5eb6f7f9d42dde97a9eb73fe627ae9
amiay <amiayn@iitbhilai.ac.in>
2020-04-16 22:56:40 +0530

    virtually working

--- control.sh
+++ control.sh
@@ -3,13 +3,29 @@
 
 # start a new enviroment this new game
 cat ./games/template > ./games/current_game
-
-
-
-mv_count=1
-human_move=$(python human_mv_detector.py) # detect the human move
+rm -r ./test_images/{imc1.png,imc2.png} || true # remove the pre-existing positions
+cp ./test_images/move_zero.png ./test_images/imc1.png  # restart move zero
+cp ./test_images/move_zero_w.png ./test_images/imc2.png
+mv_count=1 human_move=$(python human_mv_detector.py) # detect the human move 
 var=$mv_count". "$human_move" "
 echo $var >> ~/Projects/e-Fishcer/games/current_game # feed that move to a file called template
-echo $(python next_move_finder.py)>> ~/Projects/e-Fishcer/games/current_game
-
+echo $(python next_move_finder.py)>> ~/Projects/e-Fishcer/games/current_game 
 cat ./games/current_game
+
+for((mv_count=2;mv_count<=3;mv_count++))
+do
+	mv ./test_images/imc1.png ./test_images/operate/ia$mv_count.png # clean the source and store the move elsewhere
+	mv ./test_images/imc2.png ./test_images/operate/ib$mv_count.png
+	mv ./test_images/operate/im_$((mv_count*2-1)) ./test_images/imc1.png # bring next two moves to the source
+	mv ./test_images/operate/im_$((mv_count*2)) ./test_images/imc2.png
+	human_move=$(python human_mv_detector.py) # detect the human move
+	var=$mv_count". "$human_move" "
+	echo $var >> ~/Projects/e-Fishcer/games/current_game # feed that move to a file called template
+	echo $(python next_move_finder.py)>> ~/Projects/e-Fishcer/games/current_game
+	cat ./games/current_game
+
+done
+
+rm -r ./test_images/operate/* # clean the image repository
+cp  ./test_images/back_up/* ./test_images/operate/
+
